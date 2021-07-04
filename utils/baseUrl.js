1d26a9b1e22023c24749a5492560cd608a4c82da
Omar Al-Bakri <albakrio@outlook.com>
2021-07-04 09:30:12 -0700

    fixing build issues

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://inder-social-media2.herokuapp.com";
+    : "https://jovial-liskov-0f0fb7.netlify.app";
 
 module.exports = baseUrl;
